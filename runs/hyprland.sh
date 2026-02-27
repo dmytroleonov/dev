75b3ae10c07ad220bc223c05266344c4434928fc
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-02-27 19:09:14 +0200

    feat: copyq (again)

--- runs/hyprland.sh
+++ runs/hyprland.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed hyprland xdg-desktop-portal-hyprland
+sudo pacman -S --noconfirm --needed hyprland xdg-desktop-portal-hyprland wl-clip-persist
