4d99399efdcb97c171346678f9d374376218b9ac
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-03-06 02:35:11 +0200

    feat: remove autocomplete

--- runs/zsh.sh
+++ runs/zsh.sh
@@ -3,5 +3,4 @@
 sudo pacman -S --noconfirm --needed zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-yay -S --needed --noconfirm zsh-autocomplete-git
 sudo pacman -S --needed --noconfirm zsh-syntax-highlighting zsh-autosuggestions
