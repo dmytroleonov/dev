1e62c16c1f97000ec83ebba7b5e5fe4c8712dd65
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-04-24 19:21:12 +0300

    feat: bluetooth

--- runs/libs.sh
+++ runs/libs.sh
@@ -2,4 +2,5 @@
 
 sudo pacman -S --needed --noconfirm git unzip \
     noto-fonts noto-fonts-emoji noto-fonts-cjk \
-    ripgrep wl-clipboard less jq man
+    ripgrep wl-clipboard less jq man \
+    dkms linux-headers bluez bluez-utils blueman
