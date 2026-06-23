ba36ab994a215c64cb0e319a34bd175d6bf45c50
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-06-23 22:16:58 +0300

    feat: update

--- runs/libs.sh
+++ runs/libs.sh
@@ -3,4 +3,5 @@
 sudo pacman -S --needed --noconfirm git unzip \
     noto-fonts noto-fonts-emoji noto-fonts-cjk \
     ripgrep wl-clipboard less jq man \
-    dkms linux-headers bluez bluez-utils blueman
+    dkms linux-headers bluez bluez-utils blueman \
+    git-delta
