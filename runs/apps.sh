9ddcd69a5413d687e4229d9565b9b2686459d7da
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-08-16 12:37:07 +0300

    feat: vsc*de

--- runs/apps.sh
+++ runs/apps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 sudo pacman -S --noconfirm --needed telegram-desktop discord spotify-launcher \
-    libreoffice-fresh code parsec-bin
+    libreoffice-fresh parsec-bin
+yay -S visual-studio-code-bin
