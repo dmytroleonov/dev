4cfbb6134b78f16d58e29fa673f9c78ca6ed794e
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-09-24 19:35:01 +0300

    feat: fzf

--- runs/libs.sh
+++ runs/libs.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed --noconfirm git unzip \
     noto-fonts noto-fonts-emoji noto-fonts-cjk \
     ripgrep wl-clipboard less jq man \
     dkms linux-headers bluez bluez-utils blueman \
-    git-delta
+    git-delta fzf
